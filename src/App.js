979c2938dc691753608d65d3849005a9f08c00db
msebasv <msebasv@outlook.com>
2022-07-21 17:11:47 -0500

    Create layout structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Layout from "./Layout";
+
 const App = () => {
   return (
     <div className="App">
-      <h2>HOLA</h2>
+      <Layout />
     </div>
   );
 };
