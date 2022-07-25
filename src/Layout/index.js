c0ec86c5a12989d652c881c1967f8535b5b12c01
msebasv <msebasv@outlook.com>
2022-07-25 16:47:53 -0500

    Create navbar

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import NavbarMenu from "../components/NavbarMenu";
+
 import "./index.scss";
 
 const Layout = () => {
   return (
     <div className="container-layout">
-      <header>Header</header>
+      <header>
+        <NavbarMenu />
+      </header>
       <section>Section</section>
       <footer>Footer</footer>
     </div>
---
+++ src/components/NavbarMenu/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./index.scss";
+
+import FormSearch from "../FormSearch";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import Logo from "../../assets/images/logo.png";
+
+const NavbarMenu = () => {
+  return (
+    <Navbar className="navbar">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={Logo}
+            width="65"
+            height="60"
+            className="d-inline-block align-top"
+            alt="Hotels"
+          />
+        </Navbar.Brand>
+        <Nav className="nav-container">
+          <Nav.Link>Home</Nav.Link>
+          <Nav.Link>About</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarMenu;
